201b4048a665718c118ee1b5768f84c177863606
c00kamunga <tsothalanti2@yahoo.com>
2020-04-17 17:09:14 -0700

    made an api call to designated url in componentDidMount, set it to state using setState

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-
-class App extends React.Component{
-  
-  constructor(){
-    super()
+class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      soccerPlayersData: []
+      soccerPlayersData: [],
     };
   }
-  
-  
-  
-  render(){
 
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then((result) => {
+        console.log(result);
+        this.setState({
+          soccerPlayersData: result.data,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          soccerPlayersData: this.props.data,
+        });
+        console.log("error:", error);
+      });
+  }
+
+  render() {
+    return(
+      <>
+      </>
+    )
   }
 }
+
+
+export default App;
